399cc8ef8facf58af404427cbb42c017b37301ef
NJ-NXN <nixonjunior1000@gmail.com>
2026-06-27 15:00:39 +0300

    Fix tailwind issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     // This tells Vite to strip out all console.logs and debuggers during the production build
     minify: 'terser',
